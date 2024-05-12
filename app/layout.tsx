9b77c6c2f8e7e46bfca9e1e659c76009ee0d1c0f
Misha Moroshko <michael.moroshko@gmail.com>
2024-05-12 17:48:45 +1000

    Add biomejs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
