1564262c108bac8af020305099ed0a751a29f916
Misha Moroshko <michael.moroshko@gmail.com>
2024-05-19 20:37:21 +1000

    Revert "Fix og image"
    This reverts commit 7ec1a91afc8b4f65e79e8cb72e8db5347667f301.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,31 +5,12 @@ import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
-import ogImage from "../public/images/og.png";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 
 export const metadata: Metadata = {
   title: "Misha Moroshko",
   description: "Personal website of Misha Moroshko",
-  openGraph: {
-    images: [
-      {
-        url: ogImage.src,
-        width: ogImage.width,
-        height: ogImage.height,
-      },
-    ],
-  },
-  twitter: {
-    images: [
-      {
-        url: ogImage.src,
-        width: ogImage.width,
-        height: ogImage.height,
-      },
-    ],
-  },
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => {
